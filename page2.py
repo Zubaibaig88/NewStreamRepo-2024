a4819c468dc19c4a9f2ac3380ce820ab5b89a707
Zubaibaig88 <133507481+Zubaibaig88@users.noreply.github.com>
2024-09-21 16:41:25 +0300

    Create page2.py
    new page added

---
+++ page2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Title of the app
+st.title("Quote of the Day")
+
+# Display the quote
+st.write("### What gets measured, gets improved.")
+
+# Attribution
+st.write("- **Peter Drucker**")
+
